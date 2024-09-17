3d23dc37dd326528a2c91d3c958a8fb8865c9be8
相良 道広 <sagaramichihiro@sagaramichihironoMacBook-Air.local>
2024-09-17 16:42:35 +0900

    ログアウトしましたとフラッシュメッセージを表示

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,14 +7,16 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to root_path
+      redirect_to root_path, success: t('user_sessions.create.success')
     else
-      render :new
+      flash.now[:danger] = t('user_sessions.create.failure')
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
+    flash[:success] = t('user_sessions.destroy.success')  # ログアウト時のフラッシュメッセージを追加
     redirect_to root_path, status: :see_other
   end
 end
