1032e386d64b7512fa0ec041ed0e09d16a0734e3
相良 道広 <sagaramichihiro@sagaramichihironoMacBook-Air.local>
2024-09-21 02:04:42 +0900

    リベンジ

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to boards_path, success: t('user_sessions.create.success')
+      redirect_to root_path, success: t('user_sessions.create.success')  # ここを root_path に変更
     else
       flash.now[:danger] = t('user_sessions.create.failure')
       render :new, status: :unprocessable_entity
@@ -17,6 +17,6 @@ class UserSessionsController < ApplicationController
   def destroy
     logout
     flash[:success] = t('user_sessions.destroy.success')  # ログアウト時のフラッシュメッセージを追加
-    redirect_to root_path, status: :see_other
+    redirect_to root_path, status: :see_other  # ここも root_path に変更
   end
 end
