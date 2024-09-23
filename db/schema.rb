0b4a395d657f789cbf8a0fd57b7f6cd70fe18957
相良 道広 <sagaramichihiro@sagaramichihironoMacBook-Air.local>
2024-09-23 16:25:57 +0900

    add:掲示板作成画面への導線を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_23_043947) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_23_064447) do
+  create_table "articles", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "boards", charset: "utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
